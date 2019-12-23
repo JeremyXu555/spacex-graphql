595c70017d0d201131a0f44232ad4c89f37cf918
Jeremy Xu <jxu@healthhubsolutions.ca>
2019-12-22 22:57:38 -0500

    Mongodb->Postgresql:server file update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import * as express from "express"; 
 import * as graphqlHTTP from "express-graphql";
-import * as mongoose from "mongoose";
 import { generateSchema } from "./schema/index";
+import initializeSequilize from "./sequlize";
+import UmzugInstance from "./utilities/umzug";
 
 async function runServer(): Promise<void> {
   const schema = await generateSchema();
   const app = express();
+  initializeSequilize();
+
+  const migrationsUmzug = UmzugInstance('migrations');
+  try {
+      await migrationsUmzug.up();
+  }
+  catch (exception) {
+      console.log('Umzug failed to run migrations. ', exception);
+      if (process.env.NODE_ENV === 'production') {
+          throw exception;
+      }
+  }
 
   app.use(
     "/graphql",
@@ -15,8 +28,8 @@ async function runServer(): Promise<void> {
     }),
   );
 
-  mongoose.set("debug", true);
-  mongoose.connect(`mongodb://localhost:27017/graphExample`);
+  // mongoose.set("debug", true);
+  // mongoose.connect(`mongodb://localhost:27017/graphExample`);
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {console.log("server is running")});
