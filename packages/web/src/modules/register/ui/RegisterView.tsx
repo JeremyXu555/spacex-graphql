9f2c55172d05a689d5c8e97440c06208f358f8db
Jeremy Xu <jxu@healthhubsolutions.ca>
2020-01-09 13:39:56 -0500

    add in props for inpput

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Form as antdForm, Icon, Input, Button } from 'antd';
+import { Form as antdForm, Icon, Input, Button, Checkbox } from 'antd';
 import { Formik, Form } from 'formik';
 
 import "../../../index.css";
@@ -29,24 +29,32 @@ export default class RegisterView extends React.PureComponent<IRegisterViewProps
           }}
         >
           {
-            ({ values }) => (
+            ({ values, handleChange }) => (
               <Form style={{ width: 400, margin: 'auto' }}>
                 <antdForm.Item>
                   <Input
+                    name="firstName"
                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="First Name" value={values.firstName}
+                    placeholder="First Name"
+                    value={values.firstName}
+                    onChange={handleChange}
                   />
-                </antdForm.Item>
-                <antdForm.Item>
                   <Input
+                    name="lastName"
                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="Last Name" value={values.lastName}
+                    placeholder="Last Name"
+                    value={values.lastName}
+                    onChange={handleChange}
                   />
                 </antdForm.Item>
                 <antdForm.Item>
+                  <Checkbox name="crazy" checked={values.crazy} onChange={handleChange}>crazy</Checkbox>
+                  <a className="login-form-forgot" href="/">Forget password</a>
+                  <div></div>
                   <Button type="primary" htmlType="submit" className="login-form-button">
                     Log in
                   </Button>
+                  Or <a href="/">Register Now!</a>
                 </antdForm.Item>
                 <pre>
                   {JSON.stringify(values, null, 2)}
