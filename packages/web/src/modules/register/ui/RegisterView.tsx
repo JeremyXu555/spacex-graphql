7814e412fdad39d6b1efac95f03575ce785f7b10
Jeremy Xu <jxu@healthhubsolutions.ca>
2020-01-16 13:25:47 -0500

    cencertalize custom components

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -7,8 +7,11 @@ import {
   Radio,
 } from 'antd';
 import CheckboxGroup from 'antd/lib/checkbox/Group';
-import Select from '../../../components/Select';
-import Input from '../../../components/Input';
+
+import {
+  Select,
+  Input
+} from '../../../components';
 
 import {
   Formik,
@@ -91,107 +94,107 @@ export default class RegisterView extends React.PureComponent<IRegisterViewProps
           validationSchema={validationSchema}
         >
           {
-          ({ values, handleChange, errors, touched }) => (
-            <Form style={{ width: 400, margin: 'auto' }}>
-              <antdForm.Item>
-                <Input
-                  name="firstName"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="First Name"
-                  value={values.firstName}
-                  onChange={handleChange}
-                />
-                {errors.firstName && touched.firstName ? (
-                  <div>{errors.firstName}</div>
-                ) : null}
-                <Input
-                  name="lastName"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Last Name"
-                  value={values.lastName}
-                  onChange={handleChange}
-                />
-              </antdForm.Item>
-              <antdForm.Item>
-                <div style={{ borderBottom: '1px solid #E9E9E9' }}>
-                  <Checkbox
-                    indeterminate={this.state.indeterminate}
-                    onChange={this.onCheckAllChange}
-                    checked={this.state.checkAll}
-                  >
-                    Check all
+            ({ values, handleChange, errors, touched }) => (
+              <Form style={{ width: 400, margin: 'auto' }}>
+                <antdForm.Item>
+                  <Input
+                    name="firstName"
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="First Name"
+                    value={values.firstName}
+                    onChange={handleChange}
+                  />
+                  {errors.firstName && touched.firstName ? (
+                    <div>{errors.firstName}</div>
+                  ) : null}
+                  <Input
+                    name="lastName"
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Last Name"
+                    value={values.lastName}
+                    onChange={handleChange}
+                  />
+                </antdForm.Item>
+                <antdForm.Item>
+                  <div style={{ borderBottom: '1px solid #E9E9E9' }}>
+                    <Checkbox
+                      indeterminate={this.state.indeterminate}
+                      onChange={this.onCheckAllChange}
+                      checked={this.state.checkAll}
+                    >
+                      Check all
                   </Checkbox>
-                </div>
-                Hobbies:
+                  </div>
+                  Hobbies:
                 <CheckboxGroup
-                  name="hobbies"
-                  onChange={this.onHobbiesChange}
-                  options={hobbiesOptions}
-                  value={this.state.checkedHobbies}
-                />
-                <div>
-                  Which day during the week you want to work from home?
+                    name="hobbies"
+                    onChange={this.onHobbiesChange}
+                    options={hobbiesOptions}
+                    value={this.state.checkedHobbies}
+                  />
+                  <div>
+                    Which day during the week you want to work from home?
                 </div>
-                <Radio.Group
-                  name="offDay"
-                  defaultValue={values.offDay}
-                  onChange={handleChange}
-                  options={offDayOptions}
-                />
-              </antdForm.Item>
-              <FieldArray name="preferTech">
-                {
-                  arrayHelpers => (
-                    <div>
-                      <Button onClick={() => {
-                        arrayHelpers.push({
-                          framework: 'Privata',
-                          id: '' + Math.random(),
-                        })
-                      }}
-                        type='primary'
-                      >
-                        Add preference
+                  <Radio.Group
+                    name="offDay"
+                    defaultValue={values.offDay}
+                    onChange={handleChange}
+                    options={offDayOptions}
+                  />
+                </antdForm.Item>
+                <FieldArray name="preferTech">
+                  {
+                    arrayHelpers => (
+                      <div>
+                        <Button onClick={() => {
+                          arrayHelpers.push({
+                            framework: 'Privata',
+                            id: '' + Math.random(),
+                          })
+                        }}
+                          type='primary'
+                        >
+                          Add preference
                         </Button>
-                      {
-                        values.preferTech.map((tech, index) => {
-                          return (
-                            <div key={tech.id}>
-                              <Input
-                                name={`preferTech.${index}.language`}
-                                placeholder='language'
-                              />
-                              <Select name={`preferTech.${index}.framework`}>
-                                <Option value="Django">Django</Option>
-                                <Option value="Express">Express</Option>
-                                <Option value="Formik">Formik</Option>
-                              </Select>
-                              <Button type="danger" onClick={() => arrayHelpers.remove(index)}>
-                                x
+                        {
+                          values.preferTech.map((tech, index) => {
+                            return (
+                              <div key={tech.id}>
+                                <Input
+                                  name={`preferTech.${index}.language`}
+                                  placeholder='language'
+                                />
+                                <Select name={`preferTech.${index}.framework`}>
+                                  <Option value="Django">Django</Option>
+                                  <Option value="Express">Express</Option>
+                                  <Option value="Formik">Formik</Option>
+                                </Select>
+                                <Button type="danger" onClick={() => arrayHelpers.remove(index)}>
+                                  x
                                 </Button>
-                            </div>
-                          );
-                        })
-                      }
-                    </div>
-                  )
-                }
-              </FieldArray>
-              <antdForm.Item>
-                <Checkbox name="crazy" checked={values.crazy} onChange={handleChange}>crazy</Checkbox>
-                <a className="login-form-forgot" href="/">Forget password</a>
-                <div></div>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                  Log in
+                              </div>
+                            );
+                          })
+                        }
+                      </div>
+                    )
+                  }
+                </FieldArray>
+                <antdForm.Item>
+                  <Checkbox name="crazy" checked={values.crazy} onChange={handleChange}>crazy</Checkbox>
+                  <a className="login-form-forgot" href="/">Forget password</a>
+                  <div></div>
+                  <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
                 </Button>
-                Or <a href="/">Register Now!</a>
-              </antdForm.Item>
-              <pre>
-                {JSON.stringify(values, null, 2)}
-              </pre>
-            </Form>
-          )
-        }
+                  Or <a href="/">Register Now!</a>
+                </antdForm.Item>
+                <pre>
+                  {JSON.stringify(values, null, 2)}
+                </pre>
+              </Form>
+            )
+          }
         </Formik>
       </div >
     );
