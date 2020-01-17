052f5e594c90ca133266c5ee3d4a6cbaf3a7063c
Jeremy Xu <jxu@healthhubsolutions.ca>
2020-01-17 14:54:34 -0500

    introduce common package for sharing code
    tsconfig.json is used for tsc complie

---
+++ packages/common/dist/yupSchemas/user.d.ts
@@ -0,0 +1,10 @@
+import * as yup from "yup";
+export declare const emailNotLongEnough = "email must be at least 3 characters";
+export declare const passwordNotLongEnough = "password must be at least 3 characters";
+export declare const invalidEmail = "email must be a valid email";
+export declare const registerPasswordValidation: yup.StringSchema<string>;
+export declare const validUserSchema: yup.ObjectSchema<yup.Shape<object, {
+    firstName: string;
+    lastName: string;
+    password: string;
+}>>;
---
+++ packages/common/src/yupSchemas/user.ts
@@ -0,0 +1,25 @@
+import * as yup from "yup";
+
+export const emailNotLongEnough = "email must be at least 3 characters";
+export const passwordNotLongEnough = "password must be at least 3 characters";
+export const invalidEmail = "email must be a valid email";
+
+export const registerPasswordValidation = yup
+  .string()
+  .min(3, passwordNotLongEnough)
+  .max(255)
+  .required();
+
+export const validUserSchema = yup.object().shape({
+  firstName: yup
+    .string()
+    .required()
+    .max(10, 'Your fist name is too long')
+    .min(3, 'Your first name is too short haha'),
+  lastName: yup
+    .string()
+    .required()
+    .min(3, 'Nope! your last name must be longer than 3 characters')
+    .required(),
+  password: registerPasswordValidation
+});
