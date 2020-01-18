da8b1f101f65788ecfdd880447c44b4323ba4c94
Jeremy Xu <jxu@healthhubsolutions.ca>
2020-01-18 15:27:13 -0500

    include controller package

---
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IRegisterControllerProps {
+}
+
+export default class RegisterController extends React.PureComponent<IRegisterControllerProps> {
+  public render() {
+    return (
+      <div>
+        hello from controller
+      </div>
+    );
+  }
+}
